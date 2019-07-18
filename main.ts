54867ef388f781b09063e465aa10592cd8b43167
YisrealHung <43162482+YisrealHung@users.noreply.github.com>
2019-07-18 16:20:34 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,27 +1,27 @@
 //% weight=0 color=#CCB72C icon="\uf299" block="MoonCar"
 namespace mooncar {
-	pins.setPull(DigitalPin.P7, PinPullMode.PullUp)
+    pins.setPull(DigitalPin.P7, PinPullMode.PullUp)
 	pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
-	pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
+    pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
     
-	function moveMotor(num1: number, num2: number, num3: number, num4: number) {
-		pins.analogWritePin(AnalogPin.P2, num1)
-		pins.analogWritePin(AnalogPin.P8, num2)
-		pins.analogWritePin(AnalogPin.P13, num3)
-		pins.analogWritePin(AnalogPin.P14, num4)
-	}
+    function moveMotor(num1: number, num2: number, num3: number, num4: number) {
+        pins.analogWritePin(AnalogPin.P2, num1)
+        pins.analogWritePin(AnalogPin.P8, num2)
+        pins.analogWritePin(AnalogPin.P13, num3)
+        pins.analogWritePin(AnalogPin.P14, num4)
+    }
 
 	export enum Direction {
-		//% block="Forward"
-		direct1 = 1,
-		//% block="Back"
-		direct2 = 2,
-		//% block="Left"
-		direct3 = 3,
-		//% block="Right"
-		direct4 = 4,
-		//% block="Stop"
-		direct5 = 5
+			//% block="Forward"
+			direct1 = 1,
+			//% block="Back"
+			direct2 = 2,
+			//% block="Left"
+			direct3 = 3,
+			//% block="Right"
+            direct4 = 4,
+            //% block="Stop"
+            direct5 = 5
 	}
 
 	//%block="MoonCar to |move %Direction| Speed %number"
@@ -32,16 +32,16 @@ namespace mooncar {
         
         switch(direction) {
 			case 1:
-				moveMotor(1023, 1023, 0, 0)
+				moveMotor(movespeed, movespeed, 0, 0)
 				break;
 			case 2:
-				moveMotor(0, 0, 1023, 1023)
+				moveMotor(0, 0, movespeed, movespeed)
 				break;
 			case 3:
-				moveMotor(1023, 0, 0, 1023)
+				moveMotor(movespeed, 0, 0, movespeed)
 				break;
 			case 4:
-				moveMotor(0, 1023, 1023, 0)
+				moveMotor(0, movespeed, movespeed, 0)
                 break;
             case 5:
                 moveMotor(0, 0, 0, 0)
