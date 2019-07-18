204945e83d5f01b879b26c1f61e0ff34422eed8c
YisrealHung <43162482+YisrealHung@users.noreply.github.com>
2019-07-18 16:07:43 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,27 @@
 //% weight=0 color=#CCB72C icon="\uf299" block="MoonCar"
 namespace MoonCar {
-    pins.setPull(DigitalPin.P7, PinPullMode.PullUp)
+	pins.setPull(DigitalPin.P7, PinPullMode.PullUp)
 	pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
-    pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
+	pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
     
-    function moveMotor(num1: number, num2: number, num3: number, num4: number) {
-        pins.analogWritePin(AnalogPin.P2, num1)
-        pins.analogWritePin(AnalogPin.P8, num2)
-        pins.analogWritePin(AnalogPin.P13, num3)
-        pins.analogWritePin(AnalogPin.P14, num4)
-    }
+	function moveMotor(num1: number, num2: number, num3: number, num4: number) {
+		pins.analogWritePin(AnalogPin.P2, num1)
+		pins.analogWritePin(AnalogPin.P8, num2)
+		pins.analogWritePin(AnalogPin.P13, num3)
+		pins.analogWritePin(AnalogPin.P14, num4)
+	}
 
 	export enum Direction {
-			//% block="ForWard"
-			direct1 = 1,
-			//% block="Back"
-			direct2 = 2,
-			//% block="Left"
-			direct3 = 3,
-			//% block="Right"
-            direct4 = 4,
-            //% block="Stop"
-            direct5 = 5
+		//% block="Forward"
+		direct1 = 1,
+		//% block="Back"
+		direct2 = 2,
+		//% block="Left"
+		direct3 = 3,
+		//% block="Right"
+		direct4 = 4,
+		//% block="Stop"
+		direct5 = 5
 	}
 
 	//%block="MoonCar to |move %Direction| Speed %number"
