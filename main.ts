a97d327430483b0358babb92dfb2fc3fb7dfd565
YisrealHung <43162482+YisrealHung@users.noreply.github.com>
2020-07-21 16:11:47 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -536,29 +536,16 @@ namespace mooncar {
     _mode: NeoPixelMode;
     _matrixWidth: number; // number of leds in a matrix - if any
 
-    /**
-     * Shows all LEDs to a given color (range 0-255 for r, g, b).
-     * @param rgb RGB color of the LED
-     */
-    //% blockId="neopixel_set_strip_color" block="%strip|show color %rgb=neopixel_colors"
-    //% strip.defl=strip
+    //% block="%strip|show color %rgb=neopixel_colors"
     //% weight=85 blockGap=8
-    //% parts="neopixel"
     showColor(rgb: number) {
         rgb = rgb >> 0;
         this.setAllRGB(rgb);
         this.show();
     }
 
-    /**
-     * Shows a rainbow pattern on all LEDs.
-     * @param startHue the start hue value for the rainbow, eg: 1
-     * @param endHue the end hue value for the rainbow, eg: 360
-     */
-    //% blockId="neopixel_set_strip_rainbow" block="%strip|show rainbow from %startHue|to %endHue"
-    //% strip.defl=strip
+    //% block="%strip|show rainbow from %startHue|to %endHue"
     //% weight=85 blockGap=8
-    //% parts="neopixel"
     showRainbow(startHue: number = 1, endHue: number = 360) {
         if (this._length <= 0) return;
 
@@ -616,30 +603,13 @@ namespace mooncar {
         this.show();
     }
 
-    /**
-     * Converts red, green, blue channels into a RGB color
-     * @param red value of the red channel between 0 and 255. eg: 255
-     * @param green value of the green channel between 0 and 255. eg: 255
-     * @param blue value of the blue channel between 0 and 255. eg: 255
-     */
-    //% weight=1
-    //% blockId="neopixel_rgb" block="red %red|green %green|blue %blue"
-    //% advanced=true
+    //% block="red %red|green %green|blue %blue"
     export function rgb(red: number, green: number, blue: number): number {
         return packRGB(red, green, blue);
     }
-    
-    /**
-     * Displays a vertical bar graph based on the `value` and `high` value.
-     * If `high` is 0, the chart gets adjusted automatically.
-     * @param value current value to plot
-     * @param high maximum value, eg: 255
-     */
-    //% weight=84
-    //% blockId=neopixel_show_bar_graph block="%strip|show bar graph of %value|up to %high"
-    //% strip.defl=strip
-    //% icon="\uf080"
-    //% parts="neopixel"
+
+
+    //% block="%strip|show bar graph of %value|up to %high"
     showBarGraph(value: number, high: number): void {
         if (high <= 0) {
             this.clear();
@@ -668,45 +638,19 @@ namespace mooncar {
         this.show();
     }
 
-    /**
-     * Set LED to a given color (range 0-255 for r, g, b).
-     * You need to call ``show`` to make the changes visible.
-     * @param pixeloffset position of the NeoPixel in the strip
-     * @param rgb RGB color of the LED
-     */
-    //% blockId="neopixel_set_pixel_color" block="%strip|set pixel color at %pixeloffset|to %rgb=neopixel_colors"
-    //% strip.defl=strip
-    //% blockGap=8
-    //% weight=80
-    //% parts="neopixel" advanced=true
+    //% block="%strip|set pixel color at %pixeloffset|to %rgb=neopixel_colors"
+    //% weight=80 blockGap=8
     setPixelColor(pixeloffset: number, rgb: number): void {
         this.setPixelRGB(pixeloffset >> 0, rgb >> 0);
     }
 
-    /**
-     * Sets the number of pixels in a matrix shaped strip
-     * @param width number of pixels in a row
-     */
-    //% blockId=neopixel_set_matrix_width block="%strip|set matrix width %width"
-    //% strip.defl=strip
-    //% blockGap=8
-    //% weight=5
-    //% parts="neopixel" advanced=true
+    //% block="%strip|set matrix width %width"
+    //% weight=5 blockGap=8
     setMatrixWidth(width: number) {
         this._matrixWidth = Math.min(this._length, width >> 0);
     }
 
-    /**
-     * Set LED to a given color (range 0-255 for r, g, b) in a matrix shaped strip
-     * You need to call ``show`` to make the changes visible.
-     * @param x horizontal position
-     * @param y horizontal position
-     * @param rgb RGB color of the LED
-     */
-    //% blockId="neopixel_set_matrix_color" block="%strip|set matrix color at x %x|y %y|to %rgb=neopixel_colors"
-    //% strip.defl=strip
-    //% weight=4
-    //% parts="neopixel" advanced=true
+    //% block="%strip|set matrix color at x %x|y %y|to %rgb=neopixel_colors"
     setMatrixColor(x: number, y: number, rgb: number) {
         if (this._matrixWidth <= 0) return; // not a matrix, ignore
         x = x >> 0;
@@ -718,77 +662,42 @@ namespace mooncar {
         this.setPixelColor(i, rgb);
     }
 
-    /**
-     * For NeoPixels with RGB+W LEDs, set the white LED brightness. This only works for RGB+W NeoPixels.
-     * @param pixeloffset position of the LED in the strip
-     * @param white brightness of the white LED
-     */
-    //% blockId="neopixel_set_pixel_white" block="%strip|set pixel white LED at %pixeloffset|to %white"
-    //% strip.defl=strip
-    //% blockGap=8
-    //% weight=80
-    //% parts="neopixel" advanced=true
+    //% block="%strip|set pixel white LED at %pixeloffset|to %white"
+    //% weight=80 blockGap=8
     setPixelWhiteLED(pixeloffset: number, white: number): void {
         if (this._mode === NeoPixelMode.RGBW) {
             this.setPixelW(pixeloffset >> 0, white >> 0);
         }
     }
 
-    /**
-     * Send all the changes to the strip.
-     */
-    //% blockId="neopixel_show" block="%strip|show" blockGap=8
-    //% strip.defl=strip
-    //% weight=79
-    //% parts="neopixel"
+    //% block="%strip|show"
+    //% weight=79 blockGap=8
     show() {
         // only supported in beta
         // ws2812b.setBufferMode(this.pin, this._mode);
         ws2812b.sendBuffer(this.buf, this.pin);
     }
 
-    /**
-     * Turn off all LEDs.
-     * You need to call ``show`` to make the changes visible.
-     */
-    //% blockId="neopixel_clear" block="%strip|clear"
-    //% strip.defl=strip
-    //% weight=76
-    //% parts="neopixel"
+    //% block="%strip|clear"
     clear(): void {
         const stride = this._mode === NeoPixelMode.RGBW ? 4 : 3;
         this.buf.fill(0, this.start * stride, this._length * stride);
     }
 
-    /**
-     * Gets the number of pixels declared on the strip
-     */
-    //% blockId="neopixel_length" block="%strip|length" blockGap=8
-    //% strip.defl=strip
-    //% weight=60 advanced=true
+    //% block="%strip|length"
+    //% weight=60 blockGap=8
     length() {
         return this._length;
     }
 
-    /**
-     * Set the brightness of the strip. This flag only applies to future operation.
-     * @param brightness a measure of LED brightness in 0-255. eg: 255
-     */
-    //% blockId="neopixel_set_brightness" block="%strip|set brightness %brightness" blockGap=8
-    //% strip.defl=strip
-    //% weight=59
-    //% parts="neopixel" advanced=true
+    //% block="%strip|set brightness %brightness"
+    //% weight=59 blockGap=8
     setBrightness(brightness: number): void {
         this.brightness = brightness & 0xff;
     }
 
-    /**
-     * Apply brightness to current colors using a quadratic easing function.
-     **/
-    //% blockId="neopixel_each_brightness" block="%strip|ease brightness" blockGap=8
-    //% strip.defl=strip
-    //% weight=58
-    //% parts="neopixel" advanced=true
+    //% block="%strip|ease brightness"
+    //% weight=58 blockGap=8
     easeBrightness(): void {
         const stride = this._mode === NeoPixelMode.RGBW ? 4 : 3;
         const br = this.brightness;
@@ -810,16 +719,7 @@ namespace mooncar {
         }
     }
 
-    /**
-     * Create a range of LEDs.
-     * @param start offset in the LED strip to start the range
-     * @param length number of LEDs in the range. eg: 4
-     */
-    //% weight=89
-    //% blockId="neopixel_range" block="%strip|range from %start|with %length|leds"
-    //% strip.defl=strip
-    //% parts="neopixel"
-    //% blockSetVariable=range
+    //% block="%strip|range from %start|with %length|leds"
     range(start: number, length: number): Strip {
         start = start >> 0;
         length = length >> 0;
@@ -834,53 +734,29 @@ namespace mooncar {
         return strip;
     }
 
-    /**
-     * Shift LEDs forward and clear with zeros.
-     * You need to call ``show`` to make the changes visible.
-     * @param offset number of pixels to shift forward, eg: 1
-     */
-    //% blockId="neopixel_shift" block="%strip|shift pixels by %offset" blockGap=8
-    //% strip.defl=strip
-    //% weight=40
-    //% parts="neopixel"
+    //% block="%strip|shift pixels by %offset"
+    //% weight=40 blockGap=8
     shift(offset: number = 1): void {
         offset = offset >> 0;
         const stride = this._mode === NeoPixelMode.RGBW ? 4 : 3;
         this.buf.shift(-offset * stride, this.start * stride, this._length * stride)
     }
 
-    /**
-     * Rotate LEDs forward.
-     * You need to call ``show`` to make the changes visible.
-     * @param offset number of pixels to rotate forward, eg: 1
-     */
-    //% blockId="neopixel_rotate" block="%strip|rotate pixels by %offset" blockGap=8
-    //% strip.defl=strip
-    //% weight=39
-    //% parts="neopixel"
+    //% block="%strip|rotate pixels by %offset"
+    //% weight=39 blockGap=8
     rotate(offset: number = 1): void {
         offset = offset >> 0;
         const stride = this._mode === NeoPixelMode.RGBW ? 4 : 3;
         this.buf.rotate(-offset * stride, this.start * stride, this._length * stride)
     }
-
-    /**
-     * Set the pin where the neopixel is connected, defaults to P0.
-     */
-    //% weight=10
-    //% parts="neopixel" advanced=true
     setPin(pin: DigitalPin): void {
         this.pin = pin;
         pins.digitalWritePin(this.pin, 0);
-        // don't yield to avoid races on initialization
+
     }
 
-    /**
-     * Estimates the electrical current (mA) consumed by the current light configuration.
-     */
-    //% weight=9 blockId=neopixel_power block="%strip|power (mA)"
-    //% strip.defl=strip
-    //% advanced=true
+
+    //% block="%strip|power (mA)"
     power(): number {
         const stride = this._mode === NeoPixelMode.RGBW ? 4 : 3;
         const end = this.start + this._length;
@@ -1013,13 +889,8 @@ namespace mooncar {
     return packRGB(red, green, blue);
   }
 
-  /**
-   * Converts a hue saturation luminosity value into a RGB color
-   * @param h hue from 0 to 360
-   * @param s saturation from 0 to 99
-   * @param l luminosity from 0 to 99
-   */
-  //% blockId=neopixelHSL block="hue %h|saturation %s|luminosity %l"
+
+  //% block="hue %h|saturation %s|luminosity %l"
   export function hsl(h: number, s: number, l: number): number {
     h = Math.round(h);
     s = Math.round(s);
